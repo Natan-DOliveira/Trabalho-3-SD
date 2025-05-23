b94b710ca8a90139059435f4c0b9bb088ea01971
Natan Duarte de Oliveira <natanduarteoliveira@gmail.com>
2025-05-23 09:08:24 -0300

    Update top_module.sv

--- rtl/top_module.sv
+++ rtl/top_module.sv
@@ -12,3 +12,28 @@ module top_module
     output logic data_ready,
     output logic [7:0] len_out
 );    
+
+    deserializador DUT_deserializador 
+    (
+        .clock(clock),
+        .reset(reset),
+        .ack_in(ack_in),
+        .data_in(data_in),
+        .write_in(write_in),
+        .data_out(data_out),
+        .data_ready(data_ready),
+        .status_out(status_out)
+    );
+
+    fila DUT_fila
+    (
+        .clock(clock),
+        .reset(reset),
+        .len_out(len_out),
+        .data_in(data_in),
+        .data_out(data_out),
+        .enqueue_in(enqueue_in),
+        .dequeue_out(dequeue_out)
+    );
+
+endmodule
