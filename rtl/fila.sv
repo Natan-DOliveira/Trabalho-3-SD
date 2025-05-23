ceeced94f6f1322b03493569e61e5cea38528fee
Natan Duarte de Oliveira <natanduarteoliveira@gmail.com>
2025-05-23 09:01:00 -0300

    Update fila.sv

--- rtl/fila.sv
+++ rtl/fila.sv
@@ -1,11 +1,20 @@
 module fila 
-(#parameter CLOCK_10KHz = 10000)
+(#parameter CLOCK_10KHz = )
 (
+    input logic clock,
     input logic reset,
-    input logic data_in,
     input logic dequeue_in,
     input logic enqueue_in,
+    input logic[7:0] data_in,
 
-    output logic len_out,
-    output logic data_out
+    output logic[7:0] len_out,
+    output logic[7:0] data_out
 );
+
+    typedef enum logic[2:0] 
+    {
+        temp1 = 3'b000,
+        temp2 = 3'b001,
+        temp3 = 3'b010,
+        temp4 = 3'b011,
+    } state_t;
