1c8194ab2d4a2ad8607e88c5fcac830b2c0296cf
Natan Duarte de Oliveira <natanduarteoliveira@gmail.com>
2025-05-28 09:21:36 -0300

    FSM arrumada para a fila

--- rtl/fila.sv
+++ rtl/fila.sv
@@ -9,14 +9,51 @@ module fila (
     output logic [7:0] data_out
 );
 
+        // FSM
     typedef enum logic [1:0] 
     {
-        temp1 = 2'b00,
-        temp2 = 2'b01,
-        temp3 = 2'b10,
-        temp4 = 2'b11,
+        AGUARDA = 2'b00,
+        RECEBE  = 2'b01,
+        PRONTO  = 2'b10,
+        ESPERA  = 2'b11
     } state_t;
 
+        // Registradores
+    state_t state;
+    logic [2:0] head_node;    
+    logic [2:0] tail_node;
+    logic [7:0] [7:0] fila;
 
+        // Lógica Síncrona
+    always @(posedge clock_10KHz or posedge reset) begin
+        if (reset) begin
+            state     <= AGUARDA;
+            head_node <= 1'b0;
+            tail_node <= 1'b0;
+            len_out   <= 8'b0;
+            data_out  <= 8'b0;
+        end
+        else begin
+            case (state)
+                
+                AGUARDA: begin
+                    if (enqueue_in) begin
+                        state <= RECEBE;
+                    end
+                end
 
+                RECEBE: begin
+
+                end
+
+                PRONTO: begin
+
+                end
+
+                ESPERA: begin
+
+                end
+            endcase
+        end
+    end
 endmodule
